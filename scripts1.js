baaf3c265efc728294982bc0aa3b78ae20939e1a
ASHUTOSH <95875489+ashutosh1873@users.noreply.github.com>
2024-07-13 19:08:32 +0530

    Added Files For Final Search Portal
    Added Files For Final Search Portal 
    It contains both AKTU Roll No Search & Total data display.

---
+++ scripts1.js
@@ -0,0 +1,7 @@
+function handleRadioChange(event) {
+    if (event.target.value === 'search') {
+        window.location.href = 'https://ashutosh1873.github.io/Data_Search_Major_Project_b4/';
+    } else if (event.target.value === 'full') {
+        window.location.href = 'https://ashutosh1873.github.io/Data_Search_Major_Project_Table_B4/';
+    }
+}
